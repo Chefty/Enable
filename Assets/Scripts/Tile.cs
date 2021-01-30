004efcf4110774275518d0ee92c568895cbece21
Arghonot <lo.rivemale@gmail.com>
2021-01-30 11:13:24 +0000

    Addded prefabs for the placeholders of the abilities. Added a tile behavior that spawn it's ability if any.

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Ability TileOwnAbility;
+
+    private void Start()
+    {
+        if (TileOwnAbility != null)
+        {
+            TileOwnAbility.SpawnProps(transform, transform.position + (Vector3.up));
+        }
+    }
+}
