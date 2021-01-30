5e04c771e5d86721784569deb8710221edf8156c
Arghonot <lo.rivemale@gmail.com>
2021-01-30 14:57:11 +0000

    Added Water Tile script + prefab along with a Drop ability and prefab that allows the player to swim.

---
+++ Assets/Scripts/Abilities/Swim.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Swim", menuName = "Abilities/Swim")]
+public class Swim : Ability
+{
+    public override void ActionForTile() { /*nothing to do here, it's just a placeholder ability*/ }
+
+    public override void RunAction() { /*nothing to do here, it's just a placeholder ability*/ }
+
+}
