011b715253be70fd8d26038f422b652da80edbc1
Arghonot <lo.rivemale@gmail.com>
2021-01-30 11:05:05 +0000

    Added a GameManager. Added a dev scene. Added the basis for the action system. Added a scriptable object for left/right/up/down arrows. Added various materials for the test scene (to be removed later).

---
+++ Assets/Scripts/Ability.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Ability : ScriptableObject
+{
+    public GameObject PlaceHolderProps;
+    public KeyCode ActionKeycode;
+
+    GameObject _ownProps;
+    public abstract void RunAction();
+    public abstract void ActionForTile();
+    public void SpawnProps(Transform tile, Vector3 pos)
+    {
+        _ownProps = Instantiate(PlaceHolderProps, tile);
+
+        _ownProps.transform.position = pos;
+    }
+}
