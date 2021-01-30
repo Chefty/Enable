faf6b976c3a74f8ac3f824b7b75321cc3904d993
Chefty <f.cheftel@gmail.com>
2021-01-31 00:06:31 +0100

    UI Upgrade with only one tween animation for now + updating basic textures + TO IMPROVE UI render order on top when dragging

---
+++ Assets/Scripts/UI/WorldSpaceUIFaceCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldSpaceUIFaceCamera : MonoBehaviour
+{
+    private void Update() {
+        Vector3 relativePos = Camera.main.transform.position - transform.position;
+        Quaternion toRotation = Quaternion.LookRotation(relativePos);
+        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, 1 * Time.deltaTime);
+    }
+}
