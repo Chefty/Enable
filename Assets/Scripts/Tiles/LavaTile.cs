f7bf261574fb83bcf22611ef3c0ba4d4ec9fb866
Chefty <f.cheftel@gmail.com>
2021-01-31 20:58:22 +0100

    HorizontalUI + Skyboxed to customized

--- Assets/Scripts/Tiles/LavaTile.cs
+++ Assets/Scripts/Tiles/LavaTile.cs
@@ -15,14 +15,14 @@ public class LavaTile : Tile
 
     public override void TileBehaviour()
     {
-        if (GameManager.Instance.DoesPlayerPosessAbility(typeof(Shoes)))
-        {
-            return;
+        if (!GameManager.Instance.DoesPlayerPosessAbility(typeof(Shoes))) {
+            StartCoroutine(StartDeathDelayCO(.25f));
         }
-        else
-        {
+    }
 
-            GameManager.Instance.Player.gameObject.SetActive(false);
-        }
+    private IEnumerator StartDeathDelayCO(float delay) {
+        yield return new WaitForSeconds(delay);
+        GameManager.Instance.playerMovement.currentState = eState.death;
+        GameManager.Instance.onDieOnLava();
     }
 }
